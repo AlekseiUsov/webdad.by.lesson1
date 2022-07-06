f652cca42b03ca6804dae37cf4ca74a86019f4fd
Aleksei <aleksei.usoff@gmail.com>
2022-07-06 10:48:47 +0300

    feature/CT-6

--- js/script.js
+++ js/script.js
@@ -5,16 +5,30 @@ let phone = document.querySelector('.header__phone');
 let body = document.querySelector('body');
 let headerLinks = document.querySelectorAll('.header__link');
 
-headerBurger.addEventListener('click', function () {
+function toggle() {
     headerBurger.classList.toggle('active');
     header.classList.toggle('header--active');
     body.classList.toggle('body__noscroll');
     menu.classList.toggle('hidden');
     phone.classList.toggle('hidden');
-});
+}
+
+headerBurger.addEventListener('click', () => {
+    toggle();
+})
 
 headerLinks.forEach((link) => {
     link.addEventListener('click', () => {
-
+        toggle();
     })
-});
+})
+
+/* body.addEventListener('click', () => {
+    if (header.classList.contains('header--active')) {
+        toggle();
+    }
+}) */
+
+
+
+
