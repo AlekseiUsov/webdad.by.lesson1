2a7ffc295738b8f8863a1446a40c010c695e4ac6
Aleksei <aleksei.usoff@gmail.com>
2022-07-07 16:56:11 +0300

    feature/CT-6

--- js/reviews.js
+++ js/reviews.js
@@ -2,12 +2,20 @@ let reviews = document.querySelector('.reviews');
 
 reviews.addEventListener('click', (event) => {
     if (event.target.closest('.reviews__view--all')) {
-        const review = event.target.parentElement;
+        const button = event.target.parentElement;
+        const review = button.parentElement;
+
         let startReview = review.querySelector('.reviews__text');
         let endReview = review.querySelector('.reviews__text-hidden');
 
         let textStartReview = startReview.innerHTML;
         let textEndReview = endReview.innerHTML;
-        let textForAllReview = textStartReview.substing(0, textStartReview - 2);
+
+        let textAllReview = textStartReview.replace('...', ' ');
+        textAllReview = [textStartReview, textEndReview].join(' ');
+
+        const allReview = document.createElement('p');
+        allReview.innerHTML = textAllReview;
     }
 });
+
